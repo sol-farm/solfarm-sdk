b5b3583837d1b8d327f04fcfca0fdb1c1ba7e339
Barney <jiwtir69@gmail.com>
2022-01-15 22:08:42 +0530

    add program ids and oyster layouts

---
+++ constants/ids.js
@@ -0,0 +1,9 @@
+import * as anchor from '@project-serum/anchor';
+
+export const LENDING_PROGRAM_ID = new anchor.web3.PublicKey(
+  '4bcFeLv4nydFrsZqV5CgwCVrPhkQKsXtzfy2KyMz7ozM'
+);
+
+export const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
+  'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+);
